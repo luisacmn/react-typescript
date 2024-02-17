d9c80b2a59a4e70587f539feb29f130f0fbc1c53
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-02-17 17:15:53 -0300

    add: add modal structure and style

--- b3-finance/src/components/Header/Header.tsx
+++ b3-finance/src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
+import { NewTransactionModal } from "../NewTransactionModal/NewTransactionModal";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
-    return (
-        <HeaderContainer>
-            <HeaderContent>
-                B3 Finance
-                <NewTransactionButton>New Transaction</NewTransactionButton>
-            </HeaderContent>
-        </HeaderContainer>
-    )
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        B3 Finance
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New Transaction</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  );
 }
