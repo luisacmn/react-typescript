a8500d98c27e496987e1439d0b1481cfd665458f
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-02-17 14:22:01 -0300

    add: create typescript project and add global styles

---
+++ coffee-delivery/src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+    white: '#ffffff',
+  
+    'gray-100': '#FAFAFA',
+    'gray-300': '#F3F2F2',
+    'gray-400': '#EDEDED',
+    'gray-500': '#E6E5E5',
+    'gray-600': '#D7D5D5',
+    'gray-700': '#8D8686',
+    'gray-800': '#574F4D',
+    'gray-900': '#403937',
+    'gray-1000': '#272221',
+  
+    'yellow-300': '#F1E9C9',
+    'yellow-500': '#DBAC2C',
+    'yellow-700': '#C47F17',
+  
+    'purple-300': '#EBE5F9',
+    'purple-500': '#8047F8',
+    'purple-700': '#4B2995',
+  } as const
