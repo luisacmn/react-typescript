c2feb507a72d11353187d8d17447e7d928046a87
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-02-17 16:32:37 -0300

    add: Add table structure and style

---
+++ b3-finance/src/components/Table/Table.tsx
@@ -0,0 +1,28 @@
+import { PriceColor, TableContainer, TableContent } from "./styles";
+
+export function Table() {
+  return (
+    <TableContainer>
+      <TableContent>
+        <tbody>
+          <tr>
+            <td width="50%">Website Development</td>
+            <td>
+              <PriceColor variant="income">$ 12.000,00</PriceColor>
+            </td>
+            <td>Service</td>
+            <td>02/02/24</td>
+          </tr>
+          <tr>
+            <td width="50%">Dinner</td>
+            <td>
+              <PriceColor variant="outcome">- $ 200,00</PriceColor>
+            </td>
+            <td>Food</td>
+            <td>12/02/24</td>
+          </tr>
+        </tbody>
+      </TableContent>
+    </TableContainer>
+  );
+}
--- b3-finance/src/pages/Transactions/Transactions.tsx
+++ b3-finance/src/pages/Transactions/Transactions.tsx
@@ -1,11 +1,13 @@
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/Summary";
+import { Table } from "../../components/Table/Table";
 
 export function Transactions() {
-    return (
-        <div>
-            <Header />
-            <Summary />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Summary />
+      <Table />
+    </div>
+  );
 }
