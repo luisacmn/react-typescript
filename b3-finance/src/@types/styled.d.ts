f193b8b90cb3b88a799d2918c203fb80b2f08bcc
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-02-17 15:05:34 -0300

    add: add global styles and theme

---
+++ b3-finance/src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'style-components'
+import { defaultTheme } from '../styles/themes/defaultTheme'
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components'{
+    export interface DefaultTheme extends ThemeType {}
+}
