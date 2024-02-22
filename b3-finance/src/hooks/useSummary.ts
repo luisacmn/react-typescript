78a241b22126e8498bf65f3053f557304d8cabe9
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-02-21 21:54:38 -0300

    fix: remove useMemo

--- b3-finance/src/hooks/useSummary.ts
+++ b3-finance/src/hooks/useSummary.ts
@@ -1,29 +1,27 @@
-import { useContext, useMemo } from "react";
+import { useContext } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
 
 export function useSummary() {
   const { transactions } = useContext(TransactionsContext);
 
-  const summary = useMemo(() => {
-    transactions.reduce(
-      (acc, transaction) => {
-        if (transaction.type === "income") {
-          acc.income += transaction.price;
-          acc.total += transaction.price;
-        } else {
-          acc.outcome += transaction.price;
-          acc.total -= transaction.price;
-        }
-
-        return acc;
-      },
-      {
-        income: 0,
-        outcome: 0,
-        total: 0,
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
       }
-    );
-  }, [transactions]);
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
 
   return summary;
 }
