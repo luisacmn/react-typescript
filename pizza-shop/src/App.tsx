9544bb4e62da967e6a780fdbe90620ff67946ddb
Luisa Novaes <luisanovaes.arq@gmail.com>
2024-03-06 18:45:24 -0300

    add: install React Router Dom

--- pizza-shop/src/App.tsx
+++ pizza-shop/src/App.tsx
@@ -1,10 +1,7 @@
-import { Button } from "./components/ui/button";
+import { RouterProvider } from "react-router-dom";
 import "./global.css";
+import { router } from "./routes";
 
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Submit</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
---
+++ pizza-shop/src/pages/auth/sign-in.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function SignIn() {
+  return <h1>SignIn</h1>;
+}
